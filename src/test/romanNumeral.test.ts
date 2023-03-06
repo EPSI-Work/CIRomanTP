f41fff819d4405bdb1f2e817caefb8401716938f
nhuYuzuCorp <nhu@yuzucorp.com>
2023-03-06 16:23:18 +0100

    Update romanNumeral.test.ts

--- src/test/romanNumeral.test.ts
+++ src/test/romanNumeral.test.ts
@@ -4,17 +4,6 @@ import { convertToRoman } from '../romanNumeral'
 suite('convertToRoman()', () => {
     test('converts 1 to "I"', () => {
         const result = convertToRoman(1);
-        expect(result).toBe('I');
-    });
-    test('converts 2 to "II"', () => {
-        const result = convertToRoman(2);
-        expect(result).toBe('II');
-    });
-
-
-
-    test('converts 1 to "I"', () => {
-        const result = convertToRoman(1);
         expect(result).toBe('I');//cc
     });
 
